ba6fcdf030f2043d005a281fa52973cfff971bc1
naor.biton <naor.biton@walkme.com>
2021-05-11 18:59:24 +0800

    feat: translate "learn react"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('learnReact')}
         </a>
       </header>
     </div>
